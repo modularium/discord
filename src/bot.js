69b3e0a100abb2eb1c2a0508b2bd25a436325881
redcarti <ppamperr@gmail.com>
2021-08-17 17:14:44 +0500

    chore: updated bot.js

--- src/bot.js
+++ src/bot.js
@@ -4,13 +4,17 @@ const ms = require('./modules')
 
 module.exports.login = (cfg) => {
   const { options, token } = cfg.bot
+
   const bot = new Client(options || {})
+
   bot.login(token)
     .catch(({ stack }) => {
       console.log('Unable to start the bot, reason:')
       console.error(`${stack}`)
-      process.exit(1)
+      process.exit(1337)
     })
+
   delete cfg.bot.token
+
   ms(bot, cfg)
 }
